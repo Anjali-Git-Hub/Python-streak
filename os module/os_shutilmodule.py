d2c2e734feb352a8b13186d65b4378f4b4b8e16a
unknown <udemy4917@gmail.com>
2024-04-23 21:50:27 +0530

    completed os_shutil module , file I/O

---
+++ os module/os_shutilmodule.py
@@ -0,0 +1,46 @@
+import os 
+import shutil
+# os.mkdir("new folder")
+# os.rmdir("new folder")
+# shutil.rmtree("new")
+
+# open("file1.txt",'a').close()
+# print(os.getcwd())
+
+# os.mkdir("folder1")
+# if os.path.exists("folder1"):
+#     print("Already exists")
+# else:
+#     os.mkdir("folder1")
+
+# change cwd 
+# os.chdir(r'C:\Users\HP\Desktop\Python streak')
+# print(os.getcwd())
+
+
+# list all the folders and files
+# print(os.listdir())
+
+# for item in os.listdir():
+#     path=os.path.join(os.getcwd(),item)
+#     print(path)
+
+# fileiter= os.walk(r'C:\Users\HP\Desktop\Python streak\os module')
+
+# for current_path,file_list,folder_list in fileiter:
+#     print(current_path)
+#     print(file_list)
+#     print(folder_list)
+
+
+# copy a file 
+# shutil.copy('file1.txt','folder1/file.txt')
+# shutil.copytree('folder1','folder')
+# shutil.move('file1.txt','folder/file2.txt')
+
+# shutil.move('folder1','folder')
+
+# shutil.rmtree("folder")
+
+
+# os.makedirs("new/new2")
---
+++ os module/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+shutil.copy('file1.txt','folder1/file.txt')
+# shutil.copytree('folder1','folder')
